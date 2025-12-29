5fe3ace779caef8b1c95c973b2084c04b4680364
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2025-12-29 21:41:41 +0600

    understand the program structure of Java

---
+++ Java/src/ProgramStructure.java
@@ -0,0 +1,16 @@
+public class ProgramStructure {
+  static void main() {
+    System.out.println("Welcome to Java ☕");
+  }
+}
+
+/*
+  * public --> public is an access-modifier
+  * class --> class is a user defined data-type blueprint with some attributes and behaviors
+  * ProgramStructure --> ProgramStructure is the name of the class
+  * static --> Belongs to the class, can be called without creating an object
+  * void --> void is the return type.Void means null it didn't return anything
+  * System --> System is a class
+  * out.println --> helps us to print text or whatever in console
+  * ; --> it means pertinates a statement or end of line
+*/
